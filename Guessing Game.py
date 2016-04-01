d5418ae1ce34ce7ee3411d80caff1c2567d698ce
grismely <grismely.hiraldo@ms223.org>
2016-04-01 08:42:59 -0400

    Created

---
+++ Guessing Game.py
@@ -0,0 +1,11 @@
+name = raw_input("What's your name?").capitalize()
+print "Hello " + name
+maxnum = input("Whats the largest number you'd like in your range?")
+
+import random
+answer = random.randint(1,maxnum)
+print answer
+
+
+#Ask to choose from decided range
+#set up if not equal to ineqalities 
