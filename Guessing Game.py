545bb1628c866ff1f4e8224ba12b52491a624e19
grismely <grismely.hiraldo@ms223.org>
2016-04-04 08:44:07 -0400

    Update

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,11 +1,17 @@
 name = raw_input("What's your name?").capitalize()
 print "Hello " + name
-maxnum = input("Whats the largest number you'd like in your range?")
+maxnum = input("Whats the largest number you'd like to guess from?")
+userAnswer = input("Choose a number between 1 and " + str(maxnum) + "?")
 
 import random
 answer = random.randint(1,maxnum)
+
+while userAnswer != answer:
+    if userAnswer > answer:
+        print "too high!"
+    elif userAnswer < answer:
+        print "too low!"
+
+    userAnswer = input("Guess again?")
 print answer
-
-
-#Ask to choose from decided range
-#set up if not equal to ineqalities 
+#tell user if theyre correct or not
