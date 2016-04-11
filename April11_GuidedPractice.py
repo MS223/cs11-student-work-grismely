bff0e887b278c13635144f768c3246d55093b433
grismely <grismely.hiraldo@ms223.org>
2016-04-11 15:20:45 -0400

    Created

---
+++ April11_GuidedPractice.py
@@ -0,0 +1,30 @@
+# what does this function return ?
+#The function will multiple the input by 2 and then print it
+def print_only(x):
+   y = x * 2
+   print y
+
+# how is this one different ?
+#This function will be returning it rather than printing it meaning that the abswer will not be visualized
+def return_only(x):
+   y = x * 2
+   return y
+
+# let's try to use our 2 functions
+print "running print_only ..."
+print_only(7)
+
+print "running return_only ..."
+return_only(7)
+
+print "printing print_only ..."
+print print_only(7)
+
+print "printing return_only ..."
+print return_only(7)
+
+print "using print_only ..."
+print_only(7) + 6
+
+print "using return_only ..."
+return_only(7) + 6
