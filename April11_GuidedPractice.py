cd812628bbc23d828c0d32a8591a0b9aa0bf6d31
grismely <grismely.hiraldo@ms223.org>
2016-04-12 08:05:39 -0400

    Added comments

--- April11_GuidedPractice.py
+++ April11_GuidedPractice.py
@@ -8,14 +8,14 @@ def print_only(x):
 #This function will be returning it rather than printing it meaning that the abswer will not be visualized
 def return_only(x):
    y = x * 2
-   return y
+   return y #unlike the function before this one, the user will not be able to see the result because its return rather than print
 
 # let's try to use our 2 functions
 print "running print_only ..."
-print_only(7)
+print_only(7) #the input value
 
 print "running return_only ..."
-return_only(7)
+return_only(7) #the input value
 
 print "printing print_only ..."
 print print_only(7)
