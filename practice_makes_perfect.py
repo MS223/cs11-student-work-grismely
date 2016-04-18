d50be726eca57bdd44e57d1c2ba5d9ae5c610c9f
grismely <grismely.hiraldo@ms223.org>
2016-04-18 08:44:15 -0400

    Created

---
+++ practice_makes_perfect.py
@@ -0,0 +1,21 @@
+def is_even(x):
+    if x % 2 == 0:
+        print True
+     else:
+         print False
+        
+def is_int(x):
+    if x == int(x)
+        print True
+     else:
+        Print False
+
+def digit_sum(x):
+    thing = list(str(x))
+     for i in thing:
+         print thing
+
+#     sum_list = []
+#     sum_list.append(int(x))
+#     print sum(sum_list)
+digit_sum(1234)
