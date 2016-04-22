e45a22e0d1eca2716f8746f6903beef962b50e65
grismely <grismely.hiraldo@ms223.org>
2016-04-22 08:44:46 -0400

    Created

---
+++ de_vowel.py
@@ -0,0 +1,10 @@
+
+consonants = [] 
+def de_vowel(some_string):
+    for x in some_string:
+        if x != "a" and x != "e" and x != "i" and x != "o" and x != "u":
+            print x
+#.append
+#dostuff
+
+de_vowel("Hello")
