9e10fcb9313583a06517ecb9eacc423c9ac56013
grismely <grismely.hiraldo@ms223.org>
2016-05-02 08:44:41 -0400

    Created

---
+++ May 2 Quiz.py
@@ -0,0 +1,25 @@
+# def max_value(number):
+#     x = 0
+#     while x < number:
+#         print x
+#         x = x + 1
+#     print number
+# def max_value2(number):
+#   print range(1,number+1)
+#
+# max_value(45)
+
+def compare_list(list1,list2):
+    if len(list1) == len(list2):
+        for x in index(list1,list2):
+           if index.list1(x) > index.list2:
+               print index.list1(x)
+
+
+
+
+list3 = [5,6,7,8]
+list4 = [3,6,7,7]
+compare_list(list3,list4)
+
+#applywhat you talked about with SK, in notes
