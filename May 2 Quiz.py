6141d2675085d4df1ebee540f7bea6963ce3b41d
grismely <grismely.hiraldo@ms223.org>
2016-05-03 09:23:51 -0400

    Updated and Added

--- May 2 Quiz.py
+++ May 2 Quiz.py
@@ -4,22 +4,34 @@
 #         print x
 #         x = x + 1
 #     print number
-# def max_value2(number):
-#   print range(1,number+1)
-#
-# max_value(45)
+def max_value2(number):
+  print range(1,number+1)
+#prints values as a list
+max_value(45)
+
+
 
 def compare_list(list1,list2):
     if len(list1) == len(list2):
-        for x in index(list1,list2):
-           if index.list1(x) > index.list2:
-               print index.list1(x)
-
-
-
+#only if list are of the same length or index
+        for x in list1:
+            if x > list2[list1.index(x)]:
+                print x
+            else:
+                print list2[list1.index(x)]
+#runs for element of mentioned index rather than index 
 
 list3 = [5,6,7,8]
 list4 = [3,6,7,7]
 compare_list(list3,list4)
 
-#applywhat you talked about with SK, in notes
+
+def swapping_stars():
+    height = 6 #creates columns
+    width = 3 #creates rows
+    for x in range (0, height):
+        if x % 2 == 1:
+            print "- * " * width
+        else:
+            print '* - ' * width
+swapping_stars()
