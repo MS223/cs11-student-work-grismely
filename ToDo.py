aebff0d4f0bd7d9ed0080f22d3b51198ead62b8c
grismely <grismely.hiraldo@ms223.org>
2016-05-24 16:00:30 -0400

    Update

--- ToDo.py
+++ ToDo.py
@@ -1,6 +1,3 @@
-action = raw_input("What are you doing?")
-day = raw_input("What day?").capitalize()
-
 days_of_week = {
     'Monday': [] ,
     'Tuesday': [] ,
@@ -9,25 +6,29 @@ days_of_week = {
     'Friday' : [] ,
     'Saturday' : [] ,
     'Sunday' : []
-
 }
 
+
 def add():
-    action = raw_input("What are you doing?")
-    day = raw_input("What day?").capitalize()
+    action = ""
     while action != 'nothing':
+        action = raw_input("What are you doing?")
+        if action == 'nothing':
+            return None #replace with menu that refers back to first function
+        day = raw_input("What day?").capitalize()
         days_of_week[day].append(action)
-    print days_of_week
-
-add()
+        print days_of_week
 
 
-#
-# def get(day):
-#     print days_of_week[day]
-#
-# def choice():
-#     user_choice = raw_input("What do you need?")
-#     if user_choice == 'get':
-#         print day
-#         def()
+def get(day):
+    print days_of_week[day]
+
+def choice():
+    user_choice = raw_input("What do you need?")
+    if user_choice == 'get':
+        day = raw_input("What day?").capitalize()
+        get(day)
+    if user_choice == 'add':
+        add()
+
+choice()
