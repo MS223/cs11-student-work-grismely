4f032add61b27de919547c6eaca613809f7d5340
grismely <grismely.hiraldo@ms223.org>
2016-05-24 09:29:39 -0400

    Created

---
+++ ColorClass.py
@@ -0,0 +1,19 @@
+class Color(object):
+    """Represents a color"""
+c1 = Color()
+c1.r = 191
+c1.g = 84
+c1.b = 46
+
+c2 = Color()
+c2.r =
+c2.g =
+c2.b =
+
+c3 = Color()
+c3.r = 
+c3.g =
+c.b = 
+
+def add_color(color1,color2):
+    print color1 + color2 / 2
